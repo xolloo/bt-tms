f807556ff349640a4eb2ceec189a37c080a344a8
Dmitriy Amelchenko <d.a.amelchenko@outlook.com>
2023-09-28 19:08:11 +0200

    change srv

--- srv.py
+++ srv.py
@@ -1,3 +1,20 @@
 import os
 
 print(os.getcwd())
+
+
+data = {"key": "val"}
+
+
+print(data)
+
+tml = "info : {}"
+
+print(tml.format(data))
+
+tml = "info : {key}"
+
+print(tml.format(**data))
+
+
+print(tml.format(key = data["key"]))
